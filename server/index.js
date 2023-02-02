04d2b7acba638c21180c6748dfa279ec6c691ada
shajjad Shagor <shajjadshagor@gmail.com>
2023-02-02 08:45:54 -0800

    set monggose for user model and image save locally

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import { register } from "./controllers/auth.js";
 
 //** configurations */
 const __filename = fileURLToPath(import.meta.url);
@@ -35,6 +36,9 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage });
+
+//** Routes with files */
+app.post("/auth/register", upload.single("picture"), register);
 
 /* Mongoose setup*/
 const PORT = process.env.PORT || 6001;
