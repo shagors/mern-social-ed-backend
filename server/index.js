7445cb4864933b4e6fc15c76312a5d14fe36d071
shajjad Shagor <shajjadshagor@gmail.com>
2023-02-08 22:09:15 -0800

    set route fot post and likes

--- server/index.js
+++ server/index.js
@@ -9,8 +9,10 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
 import { verifyToken } from "./middleware/auth.js";
 
 //** configurations */
@@ -41,11 +43,13 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 //** Routes with files */
-app.post("/auth/register", upload.single("picture"), verifyToken, register);
+app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 //** Routes */
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 /* Mongoose setup*/
 const PORT = process.env.PORT || 6001;
